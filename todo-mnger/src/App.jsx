2a8565c1b5f6751188c8dc67710db2cb555a5164
rpaiello <rpaiello@gmail.com>
2025-10-31 11:10:07 -0400

    Nav

--- todo-mnger/src/App.jsx
+++ todo-mnger/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import ToDoApp from './components/td-app'
+import ContactForm from './components/contactform'
 import './App.css'
 
 function App() {
   return (
     <>
-      <ToDoApp />
+        <BrowserRouter>
+          <nav>
+            <Link to={"/todo"} className='active'>todo manager</Link> / {" "}
+            <Link to={"/contact"}>contact</Link>
+          </nav>
+          <br />
+          <Routes>
+            <Route path='/todo' element={<ToDoApp />}></Route>
+            <Route path='/contact' element={<ContactForm />}></Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
---
+++ todo-mnger/src/components/contactform.jsx
@@ -0,0 +1,9 @@
+function ContactForm() {
+
+return(
+    <h2>THIS IS AN EXAMPLE AND DOESN'T GO ANYWHERE</h2>
+)
+
+}
+
+export default ContactForm;
